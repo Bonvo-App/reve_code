d44fd0feb45bef378df301587daeb91ca2aadfe9
ulasala <61379162+ulasala@users.noreply.github.com>
2023-05-26 17:24:20 +0530

    resolved errors

--- reve_share_code.js
+++ reve_share_code.js
@@ -1,6 +1,5 @@
-//reve sample
 
-alert("Hello!");
+
 const data = {
     "locations" : [{
         "name":"Tampa, FL",
@@ -19,15 +18,15 @@ const data = {
         "lat":"30.3451692",
         "lng":"-82.0127616"
     }, {
-    "name":"Gainesville, FL",
-    "lat":"29.6863941",
-    "lng":"-82.4021475"
+        "name":"Gainesville, FL",
+        "lat":"29.6863941",
+        "lng":"-82.4021475"
     }, {
-    "name":"West Palm Beach, FL",
+        "name":"West Palm Beach, FL",
         "lat":"26.7420674",
         "lng":"-80.2114415"
     }, {
-    "name":"Miami, FL",
+        "name":"Miami, FL",
         "lat":"25.7825389",
         "lng":"-80.3118601"
     }]
@@ -45,7 +44,7 @@ function getDistanceFromLatLonInKm(lat1, lon1, lat2, lon2) {
     var d = R * c; // Distance in km
     return d;
 }
-​
+
 function deg2rad(deg) {
     return deg * (Math.PI/180);
 }
@@ -60,21 +59,21 @@ var inputLocation = {
 
 // Check if the input location exists in the list
 var existingLocation = data.locations.find(location => location.name === inputLocation.name);
-​
+
 if (existingLocation) {
     console.log('Input location exists in the list:', existingLocation.name);
 } else {
     // Finding closest location
     var closestLocation = null;
     var closestDistance = null;
-​
+
     for (let i = 0; i < data.locations.length; i++) {
         var location = data.locations[i];
         var distance = getDistanceFromLatLonInKm(inputLocation.lat, inputLocation.lng, location.lat, location.lng);
-​
+
         // Convert distance to miles
         var distanceInMiles = distance * 0.621371;
-​
+
         if ((closestDistance === null || distanceInMiles < closestDistance) && distanceInMiles <= 100) {
             closestLocation = location;
             closestDistance = distanceInMiles;
